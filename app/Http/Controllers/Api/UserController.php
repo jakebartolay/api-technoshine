b619c2cca4ef3a84eae957b7e077b140bb741e19
Jake Bartolay <jakebartolay147@gmail.com>
2026-03-20 22:17:52 +0800

    testing

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = User::all();
+
+        // format JSON output
+        $formattedUsers = $users->map(function ($user) {
+            return [
+                'id' => $user->id,
+                'name' => $user->name,
+                'email' => $user->email,
+                'created_at' => $user->created_at->toDateTimeString(),
+            ];
+        });
+
+        return response()->json([
+            'success' => true,
+            'data' => $formattedUsers
+        ]);
+    }
+}
